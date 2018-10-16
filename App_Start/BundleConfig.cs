bbe9a149bb8618fd2b54924297729cae6d261903
kapil-valecha <kapil.valecha@systematixindia.com>
2018-10-16 17:37:53 +0530

    updates 16/10/18

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -30,6 +30,10 @@ namespace Memberships
             bundles.Add(new ScriptBundle("~/bundles/admin").Include(
                       "~/Scripts/AdminMenu.js"));
 
+            bundles.Add(new StyleBundle("~/Content/membership").Include(
+                     "~/Content/navbar.css", 
+                     "~/Content/Thumbnails.css"));
+
         }
     }
 }
