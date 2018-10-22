ee25b4c55595e663d2d390a0a57bc0b2635e8d9e
kapil-valecha <kapil.valecha@systematixindia.com>
2018-10-22 19:53:30 +0530

    Updates || 22/10/2018

---
+++ Extensions/HttpContentExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Memberships.Extensions
+{
+    public class HttpContentExtensions
+    {
+        private const string nameIdentifier =
+            ""
+    }
+}
